729ebe12fc103a33ac93dac60c98991f6ae4ce43
gu1334 <freiregusatvo.c@gmail.com>
2025-06-03 18:53:22 -0300

    micriservice

---
+++ user-service/src/test/java/br/com/EntreQuizDev/UserServiceApplicationTests.java
@@ -0,0 +1,12 @@
+
+import br.com.EntreQuizDev.user.UserServiceApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = UserServiceApplication.class)
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
