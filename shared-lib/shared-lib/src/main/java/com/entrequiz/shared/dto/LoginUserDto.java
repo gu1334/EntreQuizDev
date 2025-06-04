e3a6ac2c635742dc5aceb91d5235cabef62ee30a
gu1334 <freiregusatvo.c@gmail.com>
2025-06-03 22:27:50 -0300

    micriservice

--- shared-lib/shared-lib/src/main/java/com/entrequiz/shared/dto/LoginUserDto.java
+++
@@ -1,9 +0,0 @@
-package br.com.entrequizdev.user.dto;
-
-public record LoginUserDto(
-
-        String email,
-        String password
-
-) {
-}
--- shared-lib/shared-lib/src/main/java/com/entrequiz/shared/dto/RecoveryJwtTokenDto.java
+++
@@ -1,8 +0,0 @@
-package br.com.entrequizdev.user.dto;
-
-public record RecoveryJwtTokenDto(
-
-        String token
-
-) {
-}
